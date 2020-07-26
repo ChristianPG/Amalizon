7d79e0ffc54dfd552c5a9db2fa0c16aafd765cda
ChristianPG <christianpg813@gmail.com>
2020-07-26 18:47:53 -0500

    Fix some spelling

--- amalizon-web/src/app/cart/cart.service.ts
+++ amalizon-web/src/app/cart/cart.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
   public cartProducts = [];
-  public numberOfProductsIncart = 0;
+  public numberOfProductsInCart = 0;
   public total = 0;
 
   constructor() {}
@@ -14,8 +14,8 @@ export class CartService {
     if (localStorage.cartProducts) {
       this.cartProducts = Object.values(JSON.parse(localStorage.cartProducts));
       this.cartProducts.map((product) => {
-        this.numberOfProductsIncart += product.quantity;
-        this.total += product.quantity * product.price
+        this.numberOfProductsInCart += product.quantity;
+        this.total += product.quantity * product.price;
       });
     } else {
       this.cartProducts = [];
@@ -45,7 +45,7 @@ export class CartService {
       [product.id]: productInCart,
     };
     this.cartProducts = Object.values(storageCartProducts);
-    this.numberOfProductsIncart += quantityToAdd;
+    this.numberOfProductsInCart += quantityToAdd;
     this.total += productInCart.price * quantityToAdd;
     localStorage.cartProducts = JSON.stringify(storageCartProducts);
   }
@@ -53,7 +53,7 @@ export class CartService {
   removeProductFromCart(productId: string) {
     let storageCartProducts = {};
     let numberOfProductsToRemove = 0;
-    let toSubstractFromTotal = 0;
+    let toSubtractFromTotal = 0;
     if (localStorage.cartProducts) {
       storageCartProducts = JSON.parse(localStorage.cartProducts);
       const productInCart = storageCartProducts[productId];
@@ -61,15 +61,15 @@ export class CartService {
         productInCart && productInCart.id === productId
           ? productInCart.quantity
           : numberOfProductsToRemove;
-      toSubstractFromTotal =
+      toSubtractFromTotal =
         productInCart && productInCart.id === productId
           ? productInCart.price * numberOfProductsToRemove
-          : toSubstractFromTotal;
+          : toSubtractFromTotal;
       delete storageCartProducts[productId];
     }
     this.cartProducts = Object.values(storageCartProducts);
-    this.numberOfProductsIncart -= numberOfProductsToRemove;
-    this.total -= toSubstractFromTotal;
+    this.numberOfProductsInCart -= numberOfProductsToRemove;
+    this.total -= toSubtractFromTotal;
     localStorage.cartProducts = JSON.stringify(storageCartProducts);
   }
 }
